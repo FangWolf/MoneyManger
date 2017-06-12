f2728efa29857ea686e705b0e29b7f36c8c5c417
lostwolf <siyu2510@Gmail.com>
2017-06-12 15:46:44 +0800

    First commit.

---
+++ app/src/main/java/com/example/siyu2/moneymanger/Welcome.java
@@ -0,0 +1,37 @@
+package com.example.siyu2.moneymanger;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo pi = pm.getPackageInfo("com.lyt.android", 0);
+            TextView versionNumber = (TextView) findViewById(R.id.versionNumber);
+            versionNumber.setText("Version " + pi.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(Welcome.this,MainActivity.class);
+                startActivity(intent);
+                Welcome.this.finish();
+            }
+
+        }, 2500);
+    }
+}

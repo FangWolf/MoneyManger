a3cb0c9d87817d1c95a2e6d16e706f7298a7557c
lostwolf <siyu2510@Gmail.com>
2017-06-13 11:20:55 +0800

    zhujiemian ok

--- app/src/main/java/com/example/siyu2/moneymanger/MainActivity.java
+++ app/src/main/java/com/example/siyu2/moneymanger/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button setting = (Button) findViewById(R.id.setting);
+        ImageButton setting = (ImageButton) findViewById(R.id.eight);
         setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {

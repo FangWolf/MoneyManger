f347d52df29e3a0b9c9367001856ceb0b5650f97
lostwolf <siyu2510@Gmail.com>
2017-06-14 16:25:32 +0800

    tongji+Fragment

--- app/src/main/java/com/example/siyu2/moneymanger/Detailall.java
+++ app/src/main/java/com/example/siyu2/moneymanger/Detailall.java
@@ -1,12 +1,15 @@
 package com.example.siyu2.moneymanger;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Detailall extends AppCompatActivity {
+public class Detailall extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,23 +18,33 @@ public class Detailall extends AppCompatActivity {
         Button btndetailget = (Button) findViewById(R.id.btnDetailget);
         Button btndetaillose = (Button) findViewById(R.id.btnDetaillose);
         Button btndetailnote = (Button) findViewById(R.id.btnDetailnote);
-        btndetailget.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(Detailall.this,"暂无",Toast.LENGTH_SHORT).show();
-            }
-        });
-        btndetaillose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(Detailall.this,"暂无",Toast.LENGTH_SHORT).show();
-            }
-        });
-        btndetailnote.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(Detailall.this,"暂无",Toast.LENGTH_SHORT).show();
-            }
-        });
+        replaceFragment(new FragmentDget());
+        btndetailget.setOnClickListener(this);
+        btndetaillose.setOnClickListener(this);
+        btndetailnote.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnDetailget:
+                replaceFragment(new FragmentDget());
+                break;
+            case R.id.btnDetaillose:
+                replaceFragment(new FragmentDlose());
+                break;
+            case R.id.btnDetailnote:
+                replaceFragment(new FragmentDnote());
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager FM = getSupportFragmentManager();
+        FragmentTransaction transaction = FM.beginTransaction();
+        transaction.replace(R.id.down,fragment);
+        transaction.commit();
     }
 }

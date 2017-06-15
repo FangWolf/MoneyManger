ff8d376a275c01a00517e66274aeaae927b8e9f6
lostwolf <siyu2510@Gmail.com>
2017-06-15 08:45:48 +0800

    denglu is ok

--- app/src/main/java/com/example/siyu2/moneymanger/Setting.java
+++ app/src/main/java/com/example/siyu2/moneymanger/Setting.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import DAO.PwdDAO;
 import model.Tb_Pwd;
@@ -25,7 +26,8 @@ public class Setting extends AppCompatActivity {
             public void onClick(View view) {
                 PwdDAO pwdDAO = new PwdDAO();
                 Tb_Pwd tb_pwd = new Tb_Pwd(txtpwd.getText().toString());
-                pwdDAO.alter(tb_pwd);// 修改用户密码
+                pwdDAO.alter(tb_pwd);// 修改密码
+                Toast.makeText(Setting.this,"密码修改成功",Toast.LENGTH_SHORT).show();
             }
         });
     }

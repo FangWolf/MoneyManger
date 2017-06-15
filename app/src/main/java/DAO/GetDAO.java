dbe758a23699355f7b224aef7f7a967ca5417afd
lostwolf <siyu2510@Gmail.com>
2017-06-15 16:58:56 +0800

    4 list demo ok

--- app/src/main/java/DAO/GetDAO.java
+++ app/src/main/java/DAO/GetDAO.java
@@ -1,6 +1,10 @@
 package DAO;
 
+import android.util.Log;
+
 import org.litepal.crud.DataSupport;
+
+import java.util.List;
 
 import model.Tb_Getmoeny;
 
@@ -14,4 +18,21 @@ public class GetDAO extends DataSupport {
         tbGetmoeny.setFlag(tb_getmoeny.getFlag());
         tbGetmoeny.save();
     }
+
+    public Tb_Getmoeny find() {
+        //查询数据
+        List<Tb_Getmoeny> allget = DataSupport.findAll(Tb_Getmoeny.class);
+        for (int i = 0;i<allget.size();i++)
+        {
+            Log.d("Detailget",allget.get(i).getFlag());
+        }
+        return allget.get(1);
+    }
+
+    /*public Tb_Getmoeny find() {
+        //查询数据
+        Tb_Getmoeny allget = DataSupport.find(Tb_Getmoeny.class,8);
+        Log.d("Detailget",allget.getFlag());
+        return allget;
+    }*/
 }
--- app/src/main/java/com/example/siyu2/moneymanger/Detailget.java
+++ app/src/main/java/com/example/siyu2/moneymanger/Detailget.java
@@ -2,6 +2,14 @@ package com.example.siyu2.moneymanger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.litepal.crud.DataSupport;
+
+import java.util.List;
+
+import DAO.GetDAO;
+import model.Tb_Getmoeny;
 
 public class Detailget extends AppCompatActivity {
 
@@ -9,5 +17,9 @@ public class Detailget extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailget);
+
+        GetDAO getDAO = new GetDAO();
+        Tb_Getmoeny tb_getmoeny = new Tb_Getmoeny();
+        getDAO.find();
     }
 }

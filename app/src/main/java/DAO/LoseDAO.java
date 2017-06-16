995b9c3f651d54977deae868e882f712a74ae41c
lostwolf <siyu2510@Gmail.com>
2017-06-16 17:48:20 +0800

    123456789 is ok

--- app/src/main/java/DAO/LoseDAO.java
+++ app/src/main/java/DAO/LoseDAO.java
@@ -2,6 +2,8 @@ package DAO;
 
 import org.litepal.crud.DataSupport;
 
+import java.util.List;
+
 import model.Tb_Losemoney;
 
 public class LoseDAO extends DataSupport {
@@ -15,4 +17,91 @@ public class LoseDAO extends DataSupport {
         tbLosemoney.save();
     }
 
+    public float shenghuo() {
+        float f = 0 ;
+        String s = "0";
+        List<Tb_Losemoney> all = DataSupport
+                .select("money")
+                .where("type = ?","生活")
+                .find(Tb_Losemoney.class);
+        for (int i=0;i<all.size();i++) {
+            s = all.get(i).getMoney();
+            float ff = Float.parseFloat(s);
+            f+=ff;
+        }
+        return f;
+    }
+
+    public float gouwu() {
+        float f = 0 ;
+        String s = "0";
+        List<Tb_Losemoney> all = DataSupport
+                .select("money")
+                .where("type = ?","购物")
+                .find(Tb_Losemoney.class);
+        for (int i=0;i<all.size();i++) {
+            s = all.get(i).getMoney();
+            float ff = Float.parseFloat(s);
+            f+=ff;
+        }
+        return f;
+    }
+
+    public float jiaofei() {
+        float f = 0 ;
+        String s = "0";
+        List<Tb_Losemoney> all = DataSupport
+                .select("money")
+                .where("type = ?","交费")
+                .find(Tb_Losemoney.class);
+        for (int i=0;i<all.size();i++) {
+            s = all.get(i).getMoney();
+            float ff = Float.parseFloat(s);
+            f+=ff;
+        }
+        return f;
+    }
+
+    public float qita() {
+        float f = 0 ;
+        String s = "0";
+        List<Tb_Losemoney> all = DataSupport
+                .select("money")
+                .where("type = ?","其他")
+                .find(Tb_Losemoney.class);
+        for (int i=0;i<all.size();i++) {
+            s = all.get(i).getMoney();
+            float ff = Float.parseFloat(s);
+            f+=ff;
+        }
+        return f;
+    }
+
+    public int max () {
+        int sum = 0;
+        float x= 0,y=0;
+        float a = shenghuo();
+        float b = gouwu();
+        float c = jiaofei();
+        float d = qita();
+        if (a>b) {
+            x=a;
+        } else {
+            x=b;
+        }
+        if (c>d) {
+            y=c;
+        } else {
+            y=d;
+        }
+        if (x>y) {
+            sum = (int)x;
+        } else {
+            sum = (int)y;
+        }
+        if (sum>999) {
+            sum/=sum;
+        }
+        return sum;
+    }
 }
--- app/src/main/java/fragment/FragmentDlose.java
+++ app/src/main/java/fragment/FragmentDlose.java
@@ -8,13 +8,40 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.siyu2.moneymanger.R;
+import com.zqx.chart.anim.Anim;
+import com.zqx.chart.data.HistogramData;
+import com.zqx.chart.view.Histogram;
+
+import DAO.LoseDAO;
 
 public class FragmentDlose extends Fragment {
+    
+    String str[] = {"生活","购物","交费","其他"};
+    LoseDAO loseDAO = new LoseDAO();
+    
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle
             savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_downlose,container,false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        float foo[] = {loseDAO.shenghuo(),loseDAO.gouwu(),loseDAO.jiaofei(),loseDAO.qita()};
+        Histogram histogramChart = (Histogram) getView().findViewById(R.id.tjt);
+        HistogramData histogramData = HistogramData.builder()
+                .setXdata(str)//
+                .setYdata(foo)
+                .setXpCount(4)
+                .setYpCount(7)
+                .setXTextSize(35)
+                .setYTextSize(40)
+                .setYMax(loseDAO.max())
+                .setAnimType(Anim.ANIM_TRANSLATE)
+                .build();
+        histogramChart.setChartData(histogramData);
     }
 }
